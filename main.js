64396342973c88060a3b4b48a7bdf95097654a61
Wellison Oliveira Silva <wonsilva100@GMAIL.COM>
2023-03-14 13:44:12 -0300

    correcao de erros

--- main.js
+++
@@ -1,9 +0,0 @@
-var criptografia = [q, a, z, w, s, x, e, d, c, r, f, v, t, g, b, y, h, n, u, j, m, i, k, o, l, p];
-var alfabeto = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-
-
-var texto = document.querySelector('#txt')
-var txt = +(texto).value
-function criptografar() {
-    console.log(txt);
-}
