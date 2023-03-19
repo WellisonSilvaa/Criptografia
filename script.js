e41dd4086e6b6f74517f9c2b104258dd8e298d9c
Wellison Oliveira Silva <wonsilva100@GMAIL.COM>
2023-03-19 18:58:01 -0300

    atualizao 19/03

--- script.js
+++ script.js
@@ -1,95 +1,89 @@
-// function criptografar(){
-//    var txt = document.getElementById('txt1')
-//     console.log(txt.value)
-
-//     // var  = txt.value;
-// };
-
 var texto2 = [];
 var mostrar = "";
 var res = "";
-var alfabetoCrip = ['q', 'a', 'z', 'w', 's', 'x', 'e', 'd', 'c', 'r', 'f', 'v', 't', 'g', 'b', 'y', 'h', 'n', 'u', 'j', 'm', 'i', 'k', 'o', 'l', 'p', '?', '*', '-', '%'];
+var alfabetoCrip = [85,93,52,8,55,41,101,72,57,54,21,23,122,70,38,40,125,17,84,16,35,2,19,68,3,30,78,123,120,97,26,102,71,117,75,108,107,103,29,47,100,112,11,86,56,5,9,7,87,24,42,95,18,81,61,22,37,115,14,88,77,116,39,44,106,111,64,48,74,121,124,45,10,118,28,34,69,58,66,99,49,32,36,60,119,82,110,1,59,53,105,46,12,43,127,50,27,31,126,33,109,94,65,20,15,67,76,113,62,98,51,114,89,4,104,90,92,63,79,25,73,6,13,96,91,83,80];
 
-var alfabeto =     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', ',', '?', '.'];
+var alfabeto =  [];
 var texto = [];
 var titulo = "";
+//---------//
+var ch = '';
+var pos = 0;
+var subst = 0;
+letraSub = "";
+var lista = document.getElementById('list')
 
 async function loadFile(file) {
   this.texto = await file.text();
   console.log(texto)
   titulo = file.name;
 
-  
-
 }
 
 function criptografar() {
   loadFile()
   let letra;
-  let alfabetoLetra;
-  let position;
-  let subst;
-  // console.log(this.alfabeto);
+  let position = 0;
+
   for(let i = 0; i < this.texto.length; i++) {
     letra = this.texto[i];
-    for (let j = 0; j < this.alfabeto.length; j++) {
-      alfabetoLetra = this.alfabeto[j];
-      if(letra == alfabetoLetra) {
-        position = j;
-        subst = this.alfabetoCrip[j];
-        
-        this.texto2.push(subst);
-        this.mostrar += this.texto2[i];
-
-      }
-      
-    }
+    console.log(letra)
+    position = letra.codePointAt(pos);
+    console.log(position)
+    subst = this.alfabetoCrip[position];
+    console.log(subst);
+    letraSub = String.fromCodePoint(+(subst))
+    console.log(letraSub);
+    this.texto2.push(letraSub);
+    // console.log(this.texto2);
+    this.mostrar += this.texto2[i];
+    // console.log(this.mostrar);
   }
-  this.mostrar += '?';
-  // console.log(this.texto2);
-  this.texto2 = [];
-  this.texto = [];
+
   this.res = this.mostrar;
-  this.texto = this.res;
-  // console.log(this.mostrar);
-  document.getElementById('output').textContent = this.res;
-  console.log(res);
-  console.log(titulo);
+  console.log(this.res);
+  // document.getElementById('output').textContent = this.res;
+  console.log('Palavra Criptografada ASCII = ', this.res);
+
+  lista = document.getElementById('list')
+  lista.innerHTML = this.res;
   
 }
 
+
+//-------- Descriptografar ----------//
 function descriptografar() {
   loadFile()
-    let letra;
-    let alfabetoLetraCrip;
-    let position;
-    let subst;
-    // console.log(this.texto[2]);
-    for(let i = 0; i < this.texto.length; i++) {
-      letra = this.texto[i];
-      for (let j = 0; j < this.alfabetoCrip.length; j++) {
-        alfabetoLetraCrip = this.alfabetoCrip[j];
-        if(letra == alfabetoLetraCrip) {
-          position = j;
-          subst = this.alfabeto[j];
-          
-          this.texto2.push(subst);
-          this.mostrar += this.texto2[i]; 
+  let letra;
+  let position = 0;
 
-        }
-        
+  for(let i = 0; i < this.texto.length; i++) {
+    letra = this.texto[i];
+    console.log('letra = ',letra);
+    position = letra.codePointAt(pos);
+    console.log('position = ', position);
+    for(let j = 0; j < alfabetoCrip.length; j++){
+      if(+(position) == alfabetoCrip[j]) {
+
+        subst = j
+        // console.log(subst);
+        letraSub = String.fromCodePoint(+(subst))
+        // console.log(letraSub);
+        this.texto2.push(letraSub);
+        // console.log(this.texto2);
+        this.mostrar += this.texto2[i];
+        // console.log(this.mostrar);
       }
     }
-    this.mostrar += ' ';
-    // console.log(this.texto2);
-    this.texto2 = [];
-    this.texto = [];
-    this.res = this.mostrar;
-    this.texto = this.res;
-    console.log(this.mostrar);
-    
-    console.log(res);
-    console.log(titulo);
+  }
+
+  this.res = this.mostrar;
+  console.log(this.res);
+  // document.getElementById('output').textContent = this.res;
+  console.log('Palavra Criptografada ASCII = ', this.res);
+
+  lista = document.getElementById('list')
+  lista.innerHTML = this.res;
   }
 
 function salvar() {
@@ -100,14 +94,54 @@ function salvar() {
 
     let blob = new Blob([texto], { type: "text/plain;charset=utf-8" });
 
-    saveAs(blob, titulos + ".txt");
+    saveAs(blob, titulos);
 
  }
 
 function limpar() {
-texto = []
-texto2 = []
-res = ""
-mostrar = ""
+  texto2 = [];
+  mostrar = "";
+  res = "";
+  texto = [];
+  titulo = "";
+  //---------//
+  ch = '';
+  pos = 0;
+  subst = 0;
+  letraSub = "";
+  lista.innerHTML = this.res;
 
+ }
+
+ 
+
+
+
+// ------ Gerar Numeros Aleatorios -----------//
+function gerandoAlfabetoBug() {
+  const maxNumbers = 127;
+let list = [];
+
+for (let i = 0; i < maxNumbers; i++) {
+   list[i] = i + 1;
+}
+
+console.log(list);
+
+let randomNumber;
+let tmp;
+
+for (let i = list.length; i;) {
+    randomNumber = Math.random() * i-- | 0;
+    tmp = list[randomNumber];
+    // troca o número aleatório pelo atual
+    list[randomNumber] = list[i];
+    // troca o atual pelo aleatório
+    list[i] = tmp;
+   var mostrarLista = list;
+}
+
+let lista = document.getElementById('list')
+lista.innerHTML = mostrarLista;
+console.log('Mostrar', mostrarLista);
  }
